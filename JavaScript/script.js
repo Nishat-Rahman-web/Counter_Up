11b171008d7037314f79ae3c857eb40c9a6c45ab
Nishat Rahman <168478182+Nishat-Rahman-web@users.noreply.github.com>
2024-05-16 06:25:18 -0700

    Update script.js

---
+++ JavaScript/script.js
@@ -0,0 +1,19 @@
+let result = document.querySelectorAll('.counter')
+
+let arr = Array.from(result)
+
+arr.map((item) =>{
+    
+    let i = 0 
+    let count = ()=>{
+        i++
+        item.innerHTML = i
+
+        if(i == item.dataset.num){
+            clearInterval(stop)
+        }
+    }
+    let stop = setInterval(()=>{
+        count()
+    },1000 / item.dataset.num)
+})
